658b0ec1da25456a6882b0d6f4b35916bed6e52b
Takuto Wada <takuto.wada@gmail.com>
2015-04-11 20:25:47 +0900

    spike

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 var Transformer = require('babel-core').Transformer;
 var t = require('babel-core').types;
 
-module.exports = new Transformer('plugin-example', {
-    FunctionDeclaration: function (node, parent) {
+function collectPath (stack, traversalPath) {
+    stack.push(traversalPath.key);
+    if (traversalPath.parentPath !== traversalPath) { // root node
+        collectPath(stack, traversalPath.parentPath);
+    }
+}
+
+function esPath (traversalPath) {
+    var stack = [];
+    collectPath(stack, traversalPath);
+    stack.reverse();
+    return stack;
+}
+
+module.exports = new Transformer('babel-plugin-espower', {
+    MemberExpression: function (node, parent, scope, file) {
+        var paths = esPath(this);
+        console.log(paths.join('/'));
+    },
+    FunctionDeclaration: function (node, parent, scope, file) {
         var id = node.id;
         node.type = 'FunctionExpression';
         node.id   = null;
