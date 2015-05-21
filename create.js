7735ed5f8e6c38660c0328404057c6497370ebd7
Takuto Wada <takuto.wada@gmail.com>
2015-05-21 10:48:11 +0900

    fix(babel-plugin-espower): eliminate babel-core dependency

--- create.js
+++ create.js
@@ -3,5 +3,5 @@
 var createEspowerVisitor = require('./lib/create-espower-visitor');
 
 module.exports = function createEspowerPlugin (babel, options) {
-    return new babel.Transformer('babel-plugin-espower', createEspowerVisitor(options));
+    return new babel.Transformer('babel-plugin-espower', createEspowerVisitor(babel, options));
 };
