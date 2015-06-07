74c6bb105b4cf753a82fa16f014c7c2ba29476af
Takuto Wada <takuto.wada@gmail.com>
2015-06-07 21:39:27 +0900

    chore(test): adjust AssignmentExpression expectation with latest babel output

--- test/fixtures/AssignmentExpression/expected.js
+++ test/fixtures/AssignmentExpression/expected.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _temp, _temp2, _temp3, _temp32;
+var _assert$_capt, _assert$_capt2, _assert$_capt3, _assert$_capt32;
 
 function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }
 
@@ -32,13 +32,13 @@ assert.strictEqual(assert._expr(assert._capt(dog.age += 1, 'arguments/0'), {
   line: 9
 }));
 
-assert(assert._expr(assert._capt((_temp = assert._capt([3], 'arguments/0/right'), _temp2 = _slicedToArray(_temp, 1), x = _temp2[0], _temp), 'arguments/0'), {
+assert(assert._expr(assert._capt((_assert$_capt = assert._capt([3], 'arguments/0/right'), _assert$_capt2 = _slicedToArray(_assert$_capt, 1), x = _assert$_capt2[0], _assert$_capt), 'arguments/0'), {
   content: 'assert([x] = [3])',
   filepath: 'test/fixtures/AssignmentExpression/fixture.js',
   line: 11
 }));
 
-assert(assert._expr(assert._capt((_temp3 = assert._capt([assert._capt(foo, 'arguments/0/right/elements/0')], 'arguments/0/right'), _temp32 = _slicedToArray(_temp3, 1), x = _temp32[0], _temp3), 'arguments/0'), {
+assert(assert._expr(assert._capt((_assert$_capt3 = assert._capt([assert._capt(foo, 'arguments/0/right/elements/0')], 'arguments/0/right'), _assert$_capt32 = _slicedToArray(_assert$_capt3, 1), x = _assert$_capt32[0], _assert$_capt3), 'arguments/0'), {
   content: 'assert([x] = [foo])',
   filepath: 'test/fixtures/AssignmentExpression/fixture.js',
   line: 13
