e7c9c6166298a47e6e9d48edfb3ea4393302a293
Takuto Wada <takuto.wada@gmail.com>
2015-11-01 22:38:06 +0900

    fix(babel-plugin-espower): dealing with Babel6 breaking changes of Property types

--- test/fixtures/AssignmentExpression/expected.js
+++ test/fixtures/AssignmentExpression/expected.js
@@ -1,9 +1,5 @@
 'use strict';
 
-var _assert$_capt, _assert$_capt2, _assert$_capt3, _assert$_capt32;
-
-var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i['return']) _i['return'](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError('Invalid attempt to destructure non-iterable instance'); } }; })();
-
 assert(assert._expr(assert._capt(counter += 1, 'arguments/0'), {
   content: 'assert(counter += 1)',
   filepath: 'test/fixtures/AssignmentExpression/fixture.js',
@@ -32,13 +28,13 @@ assert.strictEqual(assert._expr(assert._capt(dog.age += 1, 'arguments/0'), {
   line: 9
 }));
 
-assert(assert._expr(assert._capt((_assert$_capt = assert._capt([3], 'arguments/0/right'), _assert$_capt2 = _slicedToArray(_assert$_capt, 1), x = _assert$_capt2[0], _assert$_capt), 'arguments/0'), {
+assert(assert._expr(assert._capt([x] = assert._capt([3], 'arguments/0/right'), 'arguments/0'), {
   content: 'assert([x] = [3])',
   filepath: 'test/fixtures/AssignmentExpression/fixture.js',
   line: 11
 }));
 
-assert(assert._expr(assert._capt((_assert$_capt3 = assert._capt([assert._capt(foo, 'arguments/0/right/elements/0')], 'arguments/0/right'), _assert$_capt32 = _slicedToArray(_assert$_capt3, 1), x = _assert$_capt32[0], _assert$_capt3), 'arguments/0'), {
+assert(assert._expr(assert._capt([x] = assert._capt([assert._capt(foo, 'arguments/0/right/elements/0')], 'arguments/0/right'), 'arguments/0'), {
   content: 'assert([x] = [foo])',
   filepath: 'test/fixtures/AssignmentExpression/fixture.js',
   line: 13
--- test/fixtures/ObjectExpression/expected.js
+++ test/fixtures/ObjectExpression/expected.js
@@ -1,23 +1,23 @@
 'use strict';
 
 assert(assert._expr(assert._capt({ foo: assert._capt(bar, 'arguments/0/properties/0/value'), hoge: assert._capt(fuga, 'arguments/0/properties/1/value') }, 'arguments/0'), {
-  content: 'assert({foo: bar,hoge: fuga})',
+  content: 'assert({ foo: bar, hoge: fuga })',
   filepath: 'test/fixtures/ObjectExpression/fixture.js',
   line: 3
 }));
 
 assert(assert._expr(assert._capt(!assert._capt({ foo: assert._capt(assert._capt(bar, 'arguments/0/argument/properties/0/value/object').baz, 'arguments/0/argument/properties/0/value'), name: assert._capt(nameOf(assert._capt({ firstName: assert._capt(first, 'arguments/0/argument/properties/1/value/arguments/0/properties/0/value'), lastName: assert._capt(last, 'arguments/0/argument/properties/1/value/arguments/0/properties/1/value') }, 'arguments/0/argument/properties/1/value/arguments/0')), 'arguments/0/argument/properties/1/value') }, 'arguments/0/argument'), 'arguments/0'), {
-  content: 'assert(!{foo: bar.baz,name: nameOf({firstName: first,lastName: last})})',
+  content: 'assert(!{ foo: bar.baz, name: nameOf({ firstName: first, lastName: last }) })',
   filepath: 'test/fixtures/ObjectExpression/fixture.js',
   line: 5
 }));
 
 assert.deepEqual(assert._expr(assert._capt({ foo: assert._capt(bar, 'arguments/0/properties/0/value'), hoge: assert._capt(fuga, 'arguments/0/properties/1/value') }, 'arguments/0'), {
-  content: 'assert.deepEqual({foo: bar,hoge: fuga}, {hoge: fuga,foo: bar})',
+  content: 'assert.deepEqual({ foo: bar, hoge: fuga }, { hoge: fuga, foo: bar })',
   filepath: 'test/fixtures/ObjectExpression/fixture.js',
   line: 7
 }), assert._expr(assert._capt({ hoge: assert._capt(fuga, 'arguments/1/properties/0/value'), foo: assert._capt(bar, 'arguments/1/properties/1/value') }, 'arguments/1'), {
-  content: 'assert.deepEqual({foo: bar,hoge: fuga}, {hoge: fuga,foo: bar})',
+  content: 'assert.deepEqual({ foo: bar, hoge: fuga }, { hoge: fuga, foo: bar })',
   filepath: 'test/fixtures/ObjectExpression/fixture.js',
   line: 7
 }));
