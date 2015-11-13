b5f0927779cdd5adf058a109a7b0c7a22b991da6
Takuto Wada <takuto.wada@gmail.com>
2015-11-13 13:59:21 +0900

    test(babel-plugin-espower): adjust expected output with latest babel

--- test/fixtures/AwaitExpression/expected-presets-stage-3.js
+++ test/fixtures/AwaitExpression/expected-presets-stage-3.js
@@ -3,31 +3,33 @@
 var myAsync = (function () {
   var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(a) {
     return regeneratorRuntime.wrap(function _callee$(_context) {
-      while (1) switch (_context.prev = _context.next) {
-        case 0:
-          _context.t0 = assert;
-          _context.t1 = assert;
-          _context.t2 = assert;
-          _context.next = 5;
-          return a;
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            _context.t0 = assert;
+            _context.t1 = assert;
+            _context.t2 = assert;
+            _context.next = 5;
+            return a;
 
-        case 5:
-          _context.t3 = _context.sent;
-          _context.t4 = _context.t2._capt.call(_context.t2, _context.t3, 'arguments/0/left');
-          _context.t5 = _context.t4 === 3;
-          _context.t6 = _context.t1._capt.call(_context.t1, _context.t5, 'arguments/0');
-          _context.t7 = {
-            content: 'assert(await a === 3)',
-            filepath: 'test/fixtures/AwaitExpression/fixture.js',
-            line: 4,
-            async: true
-          };
-          _context.t8 = _context.t0._expr.call(_context.t0, _context.t6, _context.t7);
-          assert(_context.t8);
+          case 5:
+            _context.t3 = _context.sent;
+            _context.t4 = _context.t2._capt.call(_context.t2, _context.t3, 'arguments/0/left');
+            _context.t5 = _context.t4 === 3;
+            _context.t6 = _context.t1._capt.call(_context.t1, _context.t5, 'arguments/0');
+            _context.t7 = {
+              content: 'assert(await a === 3)',
+              filepath: 'test/fixtures/AwaitExpression/fixture.js',
+              line: 4,
+              async: true
+            };
+            _context.t8 = _context.t0._expr.call(_context.t0, _context.t6, _context.t7);
+            assert(_context.t8);
 
-        case 12:
-        case 'end':
-          return _context.stop();
+          case 12:
+          case 'end':
+            return _context.stop();
+        }
       }
     }, _callee, this);
   }));
