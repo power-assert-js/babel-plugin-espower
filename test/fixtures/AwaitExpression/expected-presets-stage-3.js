a6168f5fa7353686ff854a73442962e954c6b376
Takuto Wada <takuto.wada@gmail.com>
2015-12-06 12:36:39 +0900

    test(babel-plugin-espower): adjust expected output with latest babel

--- test/fixtures/AwaitExpression/expected-presets-stage-3.js
+++ test/fixtures/AwaitExpression/expected-presets-stage-3.js
@@ -1,5 +1,7 @@
 'use strict';
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } step("next"); }); }; }
+
 var myAsync = (function () {
   var ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee(a) {
     return regeneratorRuntime.wrap(function _callee$(_context) {
@@ -42,5 +44,3 @@ var myAsync = (function () {
 // function notAsync(a){
 //   assert((await (a)) === 3);
 // }
-
-function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { var callNext = step.bind(null, "next"); var callThrow = step.bind(null, "throw"); function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(callNext, callThrow); } } callNext(); }); }; }
