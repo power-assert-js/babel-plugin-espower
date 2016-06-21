9cd701f9b35e97d4f9dd04b79736d7f3a23f60b9
Takuto Wada <takuto.wada@gmail.com>
2016-06-21 15:02:53 +0900

    feat(babel-plugin-espower): Support SequenceExpression (i.e., comma operator)
    refs: power-assert-js/power-assert#59

---
+++ test/fixtures/SequenceExpression/fixture.js
@@ -0,0 +1,11 @@
+'use strict';
+
+assert((2, 1, 0));
+
+assert((foo, bar) === baz);
+
+assert(toto((tata, titi)));
+
+assert((foo, (bar, baz)));
+
+assert((((((foo, bar), baz), toto), tata), titi));
