541a901c29414b98f4b4f0bd8c087b2739a28679
Takuto Wada <takuto.wada@gmail.com>
2018-08-03 02:03:22 +0900

    test: add tests and jsdoc for ArgumentRecorder

--- test/recorder_test.js
+++ test/recorder_test.js
@@ -1,62 +1,105 @@
 'use strict';
 
-var PowerAssertRecorder = require('../lib/power-assert-recorder')();
+var ArgumentRecorder = require('../lib/argument-recorder')();
+
 var assert = require('assert');
 
-describe('power-assert-recorder', function () {
-    var foo = 'FOO';
+function willResolve (value) {
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            resolve(value);
+        }, 10);
+    });
+}
 
-    it('_capt', function () {
-        var _rec = new PowerAssertRecorder();
-        var identVal = _rec._capt(foo, 'arguments/0');
-        assert.equal(identVal, 'FOO');
+describe('to-be-embedded argument-recorder', function () {
+    var foo, _ar;
+    beforeEach(function () {
+        foo = 'FOO';
+        _ar = new ArgumentRecorder({
+            espath: 'arguments/0',
+            code: 'foo'
+        });
     });
 
-    it('_expr', function () {
-        var _rec = new PowerAssertRecorder();
-        var capturedExpr = _rec._expr(_rec._capt(foo, 'arguments/0'), {
-            content: 'assert(foo)',
-            filepath: 'path/to/some_test.js',
-            line: 1
+    describe('#_tap(value, espath)', function () {
+        it('returns tapped value untouched', function () {
+            var ret = _ar._tap(foo, 'arguments/0');
+            assert.equal(ret, 'FOO');
         });
-        assert.deepEqual(capturedExpr, {
-            powerAssertContext: {
-                events: [
+        it('returns argument untouched', function () {
+            var ret = _ar._tap(foo, 'arguments/0');
+            assert(ret === foo);
+        });
+    });
+
+    describe('#code()', function () {
+        it('returns canonical code of target argument', function () {
+            assert.equal(_ar.code(), 'foo');
+        });
+    });
+
+    describe('#value()', function () {
+        it('returns null if not recorded yet', function () {
+            assert.equal(_ar.value(), null);
+        });
+        it('returns recorded actual value of target argument', function () {
+            _ar._rec(_ar._tap(foo, 'arguments/0'));
+            assert.equal(_ar.value(), 'FOO');
+        });
+    });
+
+    describe('#_rec(value)', function () {
+        it('returns value untouched', function () {
+            var ret = _ar._rec(_ar._tap(foo, 'arguments/0'));
+            assert.equal(ret, 'FOO');
+        });
+        it('once called, argument value is stored', function () {
+            _ar._rec(_ar._tap(foo, 'arguments/0'));
+            assert.equal(_ar.value(), 'FOO');
+        });
+    });
+
+    describe('#eject()', function () {
+        beforeEach(function () {
+            _ar._rec(_ar._tap(foo, 'arguments/0'));
+            assert.equal(_ar.value(), 'FOO');
+        });
+        it('returns recorded values and its metadata', function () {
+            var recordedData = _ar.eject();
+            assert.deepEqual(recordedData, {
+                logs: [
                     {
                         espath: "arguments/0",
                         value: "FOO"
                     }
                 ],
                 value: "FOO"
-            },
-            source: {
-                content: "assert(foo)",
-                filepath: "path/to/some_test.js",
-                line: 1
-            }
+            });
         });
-    });
-
-    it('for asserting loop invariants, recorder should clean up captured values at the end of expression', function () {
-        var incr = (function () {
-            var cnt = 0;
-            return function () {
-                return ++cnt;
-            };
-        })();
-        var _rec = new PowerAssertRecorder();
-        var actual = [];
-        for (var i = 0; i < 3; i += 1) {
-            actual.push(_rec._expr(_rec._capt(incr(), 'arguments/0'), {
-                content: 'assert(incr())',
-                filepath: 'path/to/some_test.js',
-                line: 1
-            }));
-        }
-        assert.deepEqual(actual, [
-            {
-                powerAssertContext: {
-                    events: [
+        it('clears stored argument value', function () {
+            _ar.eject();
+            assert.equal(_ar.value(), null);
+        });
+        it('for asserting loop invariants, recorder should clean up captured values at the end of expression', function () {
+            var incr = (function () {
+                var cnt = 0;
+                return function () {
+                    return ++cnt;
+                };
+            })();
+            var _ar = new ArgumentRecorder({
+                espath: 'arguments/0',
+                code: 'incr()'
+            });
+            var actualRecordedData = [];
+            for (var i = 0; i < 3; i += 1) {
+                _ar._rec(_ar._tap(incr(), 'arguments/0'));
+                actualRecordedData.push(_ar.eject());
+            }
+            assert.deepEqual(actualRecordedData, [
+                {
+                    logs: [
                         {
                             espath: "arguments/0",
                             value: 1
@@ -64,15 +107,8 @@ describe('power-assert-recorder', function () {
                     ],
                     value: 1
                 },
-                source: {
-                    content: "assert(incr())",
-                    filepath: "path/to/some_test.js",
-                    line: 1
-                }
-            },
-            {
-                powerAssertContext: {
-                    events: [
+                {
+                    logs: [
                         {
                             espath: "arguments/0",
                             value: 2
@@ -80,29 +116,54 @@ describe('power-assert-recorder', function () {
                     ],
                     value: 2
                 },
-                source: {
-                    content: "assert(incr())",
-                    filepath: "path/to/some_test.js",
-                    line: 1
-                }
-            },
-            {
-                powerAssertContext: {
-                    events: [
+                {
+                    logs: [
                         {
                             espath: "arguments/0",
                             value: 3
                         }
                     ],
                     value: 3
-                },
-                source: {
-                    content: "assert(incr())",
-                    filepath: "path/to/some_test.js",
-                    line: 1
                 }
-            }
-        ]);
+            ]);
+        });
+    });
+
+    describe('when value is a Promise', function () {
+        beforeEach(function () {
+            foo = willResolve('future');
+        });
+        it('return value of _tap(value, espath) is still untouched', function () {
+            var ret = _ar._tap(foo, 'arguments/0');
+            assert(ret === foo);
+        });
+        it('return value of _rec(value) is still untouched', function () {
+            var ret = _ar._rec(_ar._tap(foo, 'arguments/0'));
+            assert(ret === foo);
+        });
+        it('return value of value() is still untouched', function () {
+            _ar._rec(_ar._tap(foo, 'arguments/0'));
+            assert.equal(_ar.value(), foo);
+        });
+        it('Promises in recorded logs are wrapped', function () {
+            _ar._rec(_ar._tap(foo, 'arguments/0'));
+            var recordedData = _ar.eject();
+            assert.strictEqual(recordedData.value, foo, 'return value is untouched');
+            var wrapped = recordedData.logs[0].value;
+            assert.notStrictEqual(wrapped, foo, 'Promises in logs are wrapped');
+        });
+        it('store Promise status and callback value when settled', function (done) {
+            _ar._rec(_ar._tap(foo, 'arguments/0'));
+            var recordedData = _ar.eject();
+            var wrapped = recordedData.logs[0].value;
+            assert(wrapped.status === undefined);
+            assert(wrapped.value === undefined);
+            foo.then(function (_) {
+                assert.equal(wrapped.status, 'resolved');
+                assert.equal(wrapped.value, 'future');
+                done();
+            });
+        });
     });
 
 });
