f270de97b63679c4bd620cbacc890fc67c95a75c
Takuto Wada <takuto.wada@gmail.com>
2019-01-18 09:58:32 +0900

    refactor: introduce pattern matcher object to normalize pattern configs and embed them

---
+++ test/create-pattern-matchers-test.js
@@ -0,0 +1,140 @@
+'use strict';
+
+var assert = require('assert');
+var createPatternMatchers = require('../lib/create-pattern-matchers');
+
+function withoutMatcher (p) {
+    var res = Object.assign({}, p);
+    delete res.matcher;
+    return res;
+}
+
+describe('createPatternMatchers patterns', function () {
+    it('string only pattern', function () {
+        var actual = createPatternMatchers({
+            patterns: [
+                'assert(value, [message])'
+            ]
+        }).map(withoutMatcher);
+        assert.deepStrictEqual(actual, [
+            {
+                pattern: 'assert(value, [message])',
+                index: 0,
+                args: [
+                    {
+                        name: 'value',
+                        index: 0,
+                        kind: 'mandatory'
+                    },
+                    {
+                        name: 'message',
+                        index: 1,
+                        kind: 'optional',
+                        message: true
+                    }
+                ]
+            }
+        ]);
+    });
+
+    it('pattern object', function () {
+        var actual = createPatternMatchers({
+            patterns: [
+                {
+                    pattern: 'assert.deepEqual(actual, expected, [message])',
+                    args: [
+                        { name: 'actual', options: {depth: 2 }},
+                        { name: 'expected', options: {depth: 2 }},
+                        { name: 'message', message: true },
+                    ]
+                }
+            ]
+        }).map(withoutMatcher);
+        assert.deepStrictEqual(actual, [
+            {
+                pattern: 'assert.deepEqual(actual, expected, [message])',
+                index: 0,
+                args: [
+                    {
+                        name: 'actual',
+                        index: 0,
+                        kind: 'mandatory',
+                        options: { depth: 2 }
+                    },
+                    {
+                        name: 'expected',
+                        index: 1,
+                        kind: 'mandatory',
+                        options: { depth: 2 }
+                    },
+                    {
+                        name: 'message',
+                        index: 2,
+                        kind: 'optional',
+                        message: true
+                    },
+                ]
+            }
+        ]);
+    });
+
+    it('mixture of strings and objects', function () {
+        var actual = createPatternMatchers({
+            patterns: [
+                'assert(value, [message])',
+                {
+                    pattern: 'assert.deepEqual(actual, expected, [message])',
+                    args: [
+                        { name: 'actual', options: {depth: 2 }},
+                        { name: 'expected', options: {depth: 2 }},
+                        { name: 'message', message: true },
+                    ]
+                }
+            ]
+        }).map(withoutMatcher);
+        assert.deepStrictEqual(actual, [
+            {
+                pattern: 'assert(value, [message])',
+                index: 0,
+                args: [
+                    {
+                        name: 'value',
+                        index: 0,
+                        kind: 'mandatory'
+                    },
+                    {
+                        name: 'message',
+                        index: 1,
+                        kind: 'optional',
+                        message: true
+                    }
+                ]
+            },
+            {
+                pattern: 'assert.deepEqual(actual, expected, [message])',
+                index: 1,
+                args: [
+                    {
+                        name: 'actual',
+                        index: 0,
+                        kind: 'mandatory',
+                        options: { depth: 2 }
+                    },
+                    {
+                        name: 'expected',
+                        index: 1,
+                        kind: 'mandatory',
+                        options: { depth: 2 }
+                    },
+                    {
+                        name: 'message',
+                        index: 2,
+                        kind: 'optional',
+                        message: true
+                    },
+                ]
+            }
+        ]);
+    });
+
+});
